77eebe93e833abbc1fa4eee30eebe7fdf09d144d
jneumaier <jneumaier@users.noreply.github.com>
2018-09-17 23:26:23 +0200

    Release version 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='krakenx',
-    version='0.0.2',
+    version='0.0.3',
     packages=['krakenx'],
     scripts=['bin/colctl', 'bin/colctl.py'],
     install_requires=['pyusb'],
