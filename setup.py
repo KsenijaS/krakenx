798440e390ade9d69d0365b5d604859d52a95b83
Jonas Malaco <jonas@protocubo.io>
2020-10-27 08:13:25 -0300

    Remove unnecessary direct dependency to PyUSB
    This is taken care of by liquidctl and, in fact, Kraken 2 devices are
    now accessed through HIDAPI on all platforms.

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.0.3',
     packages=['krakenx'],
     scripts=['bin/colctl', 'bin/colctl.py'],
-    install_requires=['pyusb', 'liquidctl>=1.2.0'],
+    install_requires=['liquidctl>=1.2.0'],
 )
